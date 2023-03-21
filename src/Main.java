9eaf2dda533d40610b612e700299d02bf057be03
Karina <bagdasarova-k@inbox.ru>
2023-03-21 19:46:17 +0500

    hW11 methods (change )

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+
+import static java.time.LocalDate.now;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,51 +9,43 @@ public class Main {
         task2();
         task3();
     }
+          public static void task1() {
+              System.out.println("Задание 1");
+              int year = 2017;
+              printYear1(year);
+          }
 
-    public static void printYear1(int yearYears) {
-        System.out.println(yearYears + " год является високосным ");
-    }
-    public static void printYear2(int yearYears2) {
-        System.out.println(yearYears2 + " год  не является високосным");
-    }
-
-
-    public static void task1() {
-        System.out.println("Задание 1");
-        int year = 1998;
+    public static void printYear1(int year) {
         if ( (year % 4 == 0 && year % 100 != 0) ||  (year % 400 == 0)) {
-            printYear1(year);
-        } if ((year % 400 != 0 && year % 100 != 0) ||  (year % 400 != 0))  {
-            printYear2(year);
-        }
-        }
-
-
-
-    public static int getClientOS(String name) {
-        if (name.equals("iOS")) {
-            return 1;
+            System.out.println(year + "год является високосным");
         } else {
-            return 0;
+            System.out.println(year + "год  не является високосным");
+
         }
     }
 
     public static void task2() {
         System.out.println("Задание 2");
-        String clientOs0 = "iOS";
-        int clientOs = getClientOS(clientOs0);
-        int clientDeviceYear = 2016;
-        if (clientDeviceYear < 2015 && clientOs == 0) {
-               System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-         } else if (clientDeviceYear > 2015 && clientOs == 0)  {
-           System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear < 2015 && clientOs == 1) {
-          System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } if (clientDeviceYear > 2015 && clientOs == 1){
-          System.out.println("Установите версию приложения для Android по ссылке");
-        }
+        getClientOS(1, 2016);
        }
 
+    public static void getClientOS(int ios, int clientOs) {
+        int currentYear = LocalDate.now().getYear();
+        if (ios == 0) {
+            if (clientOs < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOs == currentYear) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (ios == 1) {
+            if (clientOs < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+            if (clientOs == currentYear) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }
 
        public static int countDate (int deliveryDistance) {
            if (deliveryDistance < 0) {
